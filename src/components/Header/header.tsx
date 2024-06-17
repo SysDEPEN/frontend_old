8eff0020bb205d07af5418ccc08326357f2ea2d4
Kauã Werle <kauawerle19@gmail.com.br>
2024-06-17 19:37:51 -0300

    fix

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,6 +1,14 @@
+'use client'
 import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
 
 export default function HeaderComponent() {
+  const [userLogged, setUserLogged] = useState(true);
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      setUserLogged(true);
+    }
+  });
   return (
     <section className="bg-[#0135BA]">
       <div className="flex">
@@ -13,13 +21,23 @@ export default function HeaderComponent() {
           <a href="/sendform" className="py-4 px-4 uppercase">
             Documentos
           </a>
-          <a href="/listProtocols" className="py-4 px-4 uppercase">Sobre</a>
+          <a href="/listProtocols" className="py-4 px-4 uppercase">
+            Sobre
+          </a>
         </div>
         <div className="flex px-2 items-center justify-end">
-          <button className="flex items-center shadow-lg justify-center p-4 bg-[#1348D0] rounded-md w-[177px] h-[39px]">
-            <a href="/login" className="uppercase mr-10">Entrar</a>
-            <ArrowRightCircleIcon className="h-8 w-8" />
-          </button>
+          {userLogged ? (
+            <h3 className="flex items-center shadow-lg justify-center p-4 bg-[#1348D0] rounded-md w-[177px] h-[39px]">
+              Thierre Tries
+            </h3>
+          ) : (
+            <button className="flex items-center shadow-lg justify-center p-4 bg-[#1348D0] rounded-md w-[177px] h-[39px]">
+              <a href="/login" className="uppercase mr-10">
+                Entrar
+              </a>
+              <ArrowRightCircleIcon className="h-8 w-8" />
+            </button>
+          )}
         </div>
       </div>
     </section>
